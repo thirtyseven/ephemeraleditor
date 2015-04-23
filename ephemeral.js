e50c5d2acfdfb6bbd3f996a9befb5befefbe9e30
Edward Kaplan <ekaplan@mdlogix.com>
2015-04-22 23:57:40 -0700

    add splash screen

--- ephemeral.js
+++ ephemeral.js
@@ -1,32 +1,52 @@
-var element;
-function update() {
-  var text = element.value;
+var Animations = {
+  FAST: 'fade-fast',
+  SLOW: 'fade'
+};
+
+function typeChar(char, anim, elem) {
   var span = document.createElement('span');
-  span.textContent = text;
-  span.className = 'fade';
-  element.value = '';
-  var ps = document.getElementsByTagName('p');
-  ps[ps.length-1].appendChild(span);
+  span.textContent = char; 
+  span.className = anim;
+  elem.appendChild(span);
 }
-window.onload = function(){ 
-  element = document.getElementById('hidden');  
-  document.addEventListener('mousedown', function(event) {
-    console.log('focus');
-    event.stopPropagation();
-    event.preventDefault();
-    element.focus();
+
+function doSplash() {
+  var splashElem = document.getElementsByClassName('splash')[0];
+  var ps = splashElem.getElementsByTagName('p');
+  var t = 0;
+  var texts = Array.prototype.map.call(ps, function(p) {
+    var val = p.textContent;
+    p.textContent = '';
+    return val;
   });
-  element.addEventListener('keypress', function(event) {
+  texts.forEach(function(text, i) {
+    var p = ps[i];
+    Array.prototype.forEach.call(text, function(c) {
+      setTimeout(function() {
+        typeChar(c, Animations.FAST, p);
+      }, t*100);
+      t++;
+    });
+  });
+}
+
+function newLine() {
+  var newP = document.createElement('p');
+  document.body.appendChild(newP);
+  return newP;
+}
+
+window.onload = function(){ 
+  var p = newLine();
+  document.addEventListener('keypress', function(event) {
     if (event.keyCode == 13) {
       event.preventDefault();
       event.stopPropagation();
-      var newP = document.createElement('p');
-      document.body.appendChild(newP);
+      p = newLine();
     } else {
-      update();
+      typeChar(String.fromCharCode(event.which), Animations.SLOW, p);
     }
   });
-  //document.addEventListener('keyup', simulateEvent(eventName, element, false));
+
+  doSplash();
 }
-
-
